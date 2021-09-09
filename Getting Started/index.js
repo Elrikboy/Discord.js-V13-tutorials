e8ab27b839c8f38ca5b6a4df24d9487f83ccea07
Elrikboy <88180933+Elrikboy@users.noreply.github.com>
2021-09-09 18:48:58 +0530

    Create index.js

---
+++ Getting Started/index.js
@@ -0,0 +1,18 @@
+const Discord =  require('discord.js')
+const { Client, Intents } = require('discord.js');
+const client = new Client({ 
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  
+});
+
+
+
+
+
+
+client.login(process.env.token)
+
+//NEXT I'LL TEACH YOU TO MAKE COMMANDS
