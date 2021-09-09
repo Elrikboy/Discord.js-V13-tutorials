556e9a724ed42173093cf616d1056fdd528b8787
Elrikboy <88180933+Elrikboy@users.noreply.github.com>
2021-09-09 18:57:53 +0530

    Update index.js

--- Getting Started/index.js
+++ Getting Started/index.js
@@ -3,7 +3,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ 
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require('./config.js')
-const prefix = require(./config.js').prefix
+const prefix = require('./config.js').prefix
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
