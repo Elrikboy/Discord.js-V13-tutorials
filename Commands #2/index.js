3d2c17f8790d546c47b898512c3f139d0008f30f
Elrik <88180933+Elrikboy@users.noreply.github.com>
2021-09-09 19:13:25 +0530

    Create index.js

---
+++ Commands #2/index.js
@@ -0,0 +1,31 @@
+const Discord =  require('discord.js')
+const { Client, Intents } = require('discord.js');
+const client = new Client({ 
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const config = require('./config.js')
+const prefix = require('./config.js').prefix
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  
+});
+
+client.on('messageCreate', message => {
+  if (message.content === prefix + 'ping') {
+    message.channel.send('PING: ' + client.ws.ping);
+  }
+});
+
+client.on('messageCreate', message => {
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+  if (message.content === prefix + 'say') {
+    message.channel.send(args.join(" ");
+  }
+});
+
+
+client.login(config.token)
+
+//NEXT I'LL TEACH YOU TO MAKE COMMANDS
